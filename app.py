195c8914e9dad8a21ec4e5e7f9551cc006beff19
basharaz1 <basharalli@h-MacBook-Pro-sl-Bashar.local>
2024-04-10 09:42:53 +0300

    init model and add simple html

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import os
-from flask import Flask, request
+from flask import Flask, request,render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import pandas as pd
+from flask import jsonify
+import pickle
 
 db_username = os.environ['DB_USERNAME']
 db_password = os.environ['DB_PASSWORD']
@@ -14,35 +17,77 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 db = SQLAlchemy()
 db.init_app(app)
-class User(db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(50), nullable=False)
 
-    def __init__(self, name):
-        self.name = name
+class BaseballData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    League = db.Column(db.String(50))
+    Year = db.Column(db.Integer)
+    OBP = db.Column(db.Float)
+    SLG = db.Column(db.Float)
+    BA = db.Column(db.Float)
+    Playoffs = db.Column(db.Integer)
+    G = db.Column(db.Integer)
+    OOBP = db.Column(db.Float)
+    OSLG = db.Column(db.Float)
+    RD = db.Column(db.Integer)
 
-@app.route("/")
-def home():
-    return "Hello from my Containerized Server"
 
-@app.route('/users', methods=['POST'])
-def add_user():
-    request_data = request.get_json()
-    u_name = request_data['name']
-    new_user = User(
-        name=u_name)
-    db.session.add(new_user)
+def load_model_components():
+    global svm_model
+    with open('svm_model.pkl', 'rb') as f:
+        svm_model = pickle.load(f)
+
+@app.route('/')
+def homepage():
+    return render_template("index.html")
+@app.route('/predict', methods=['POST'])
+def predict():
+    load_model_components()
+
+    league = request.form['league']
+    if league=='NL':
+        league=1
+    else:
+        league=0   
+    year = request.form['year']
+    obp = request.form['obp']
+    slg = request.form['slg']
+    ba = request.form['ba']
+    g = request.form['g']
+    oobp = request.form['oobp']
+    oslg = request.form['oslg']
+    rd = request.form['rd']
+    input_data = pd.DataFrame({
+
+        'Year': [year],
+        'OBP': [obp],
+        'SLG': [slg],
+        'BA': [ba],
+        'G': [g],
+        'OOBP': [oobp],
+        'OSLG': [oslg],
+        'RD': [rd],
+        'League_NL': [league],
+    })
+    prediction = svm_model.predict(input_data)
+    baseball_data = BaseballData(
+        League='NL' if league == 1 else 'AL',
+        Year=int(year),
+        OBP=float(obp),
+        SLG=float(slg),
+        BA=float(ba),
+        Playoffs=int(prediction),
+        G=int(g),
+        OOBP=float(oobp),
+        OSLG=float(oslg),
+        RD=int(rd)
+    )
+    db.session.add(baseball_data)
     db.session.commit()
-    return "User added successfully"
-
-@app.route('/users')
-def show_users():
-    users = User.query.all()
-    user_list = {}
-    for user in users:
-        user_list[user.id] = user.name
-    return user_list
+    return render_template('result.html', prediction=prediction)
+    
+  
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(host='0.0.0.0', port=5555)
